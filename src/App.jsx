5ba30785e434a0c4ed8ce396698147c57c2a8fdd
unknown <aishwaryagorla1231@gmail.com>
2024-10-05 09:15:58 +0530

    Birthday

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './index.css';
+function App() {
+    let [data,setData]=useState(db);
+    return (
+       <div id="body">
+         <div id="conatiner">
+            <h3 style={{textAlign:"center", paddingBottom:"10px",color:"white"}}>{data.length} Birthdays Today</h3>
+            {data.map((x, index) => {
+                return (
+                    <div key={index} id="main">
+                      
+                       <img src={x.img} alt="images" style={{height:"50px", width:"50px", borderRadius:"50%"}}/>
+                      
+                     <ruby style={{color:"white" , fontSize:"6vh",fontFamily:"arial-bold"}}>
+                    {x.Name} <rt>{x.Age}</rt>
+                     </ruby>
+                  
+                    </div>
+                    
+                );
+            })}
+            <button onClick={()=>setData([])}>Clear All</button>
+        </div>
+       </div>
+    );
+}
+
+export default App;
